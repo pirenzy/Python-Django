79404d552cb266a9102f50dd70173af33d740a71
lee myungheon <pirenzy94@gmail.com>
2018-07-31 21:27:06 +0900

    Add project_1-1.py
     - recommend menu

--- project_1-1.py
+++ project_1-1.py
@@ -1,16 +1,24 @@
 import random
 
-korea = ["반기다", "한식좋아", "만남류"]
-china = ["선혜향", "즐겨찾기", "중화맛"]
-japan = ["헬로우스시", "해피초밥", "서다우"]
+# 1) 리스트를 만든다.
+korean_food = ("김치찌개", "비빔밥", "국수")
+chinese_food = ("짜장면", "탕수육", "짬뽕")
+japanese_food = ("라멘", "돈까스", "돈부리")
 
-a = input("한식, 중식, 일식 중 한 가지를 고르세요(맛집추천) : ")
+# 2) 사용자로부터 입력
+user_choice = input("한식, 중식, 일식 중 한 가지를 고르세요(맛집추천) : ")
 
-if a == "한식":
-    print(random.choice(korea))
-elif a == "중식":
-    print(random.choice(china))
-elif a == "일식":
-    print(random.choice(japan))
+# 3) 임의로 추천
+
+
+if user_choice == "한식":
+    choice_result = random.choice(korean_food)
+elif user_choice == "중식":
+    choice_result = random.choice(chinese_food)
+elif user_choice == "일식":
+    choice_result = random.choice(japanese_food)
 else:
-    print("값을 잘못 입력하셨습니다.")
+    print("한식, 중식, 일식 중에서 선택하셔야 됩니다.")
+
+if choice_result:
+    print("{} 중에서 {}가 선택 되었습니다.".format(user_choice, choice_result))
