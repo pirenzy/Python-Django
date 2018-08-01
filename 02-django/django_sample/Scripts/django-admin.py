0868bb378bf50c284250cf522997d178e0372fd2
lee myungheon <pirenzy94@gmail.com>
2018-08-01 20:45:21 +0900

    02-django

---
+++ 02-django/django_sample/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lmh59\dev\python-django\02-django\django_sample\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
