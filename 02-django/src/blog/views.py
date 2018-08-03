8841e955ac0511c7860e6b4264244b0b5dc41633
lee myungheon <pirenzy94@gmail.com>
2018-08-03 18:53:44 +0900

    admin

--- 02-django/src/blog/views.py
+++ 02-django/src/blog/views.py
@@ -1,11 +1,21 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 # from random import randint
+from .models import Article
 # Create your views here.
-
 def index(request):
     # random_number = randint(1, 10)
     # return HttpResponse("Hello, world. {}".format(random_number))
     # return HttpResponse("Hello, world. You're at the index.")
-    name = "pirenzy"
-    return render(request, "index.html", { "name" : name })
+    # name = "pirenzy"
+    # return render(request, "index.html", { "name" : name })
+    article_list = Article.objects.all()
+    # Article.objects.create(
+    #     title="hello",
+    #     contents="this is test",
+    #     view_count = 0
+    # )
+    ctx = {
+        "article_list" : article_list
+    }
+    return render(request, "index.html", ctx)
--- 02-django/src/sample/settings.py
+++ 02-django/src/sample/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # my apps
-    'blog'
+    'blog',
 ]
 
 MIDDLEWARE = [
